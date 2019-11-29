269ab2afbb7b44b7cb8cb1ef4172785c355154f7
Daniel Q Parra <queraltoies@gmail.com>
2019-11-29 12:31:58 +0100

    Implementacion de movimiento
    Se implementan funciones para crear los objetos marcianos y ponerlos en movimiento, a su vez se controla si alguno de ellos toca el final de la pantalla y se sale del intervalo

--- clases/Juego.js
+++ clases/Juego.js
@@ -7,5 +7,82 @@ export class Juego
     constructor()
     {
 
+        this.limiteVentanaH = contenedor.clientWidth;
+        this.limiteVentanaV = contenedor.clientHeight;
+        this.marciano = []; 
+        
+        this.crearMarcianos();
+        console.log(this.marciano);
+
+        this.inicio();
+    }
+
+    crearMarcianos()//funcion que crea los marcianos
+    {
+        let posIni=0;
+
+        for (let index = 0; index < 15; index++) 
+        {
+            if (index<5) {
+                this.marciano[index] = new Marciano(this.limiteVentanaH,this.limiteVentanaV,"contenedor","marciano"+index,50,50,posIni);
+                posIni+=100;
+                if (index==4) {
+                    posIni=0;
+                }
+            }else if (index >= 5 && index < 10) {                
+                this.marciano[index] = new Marciano(this.limiteVentanaH,this.limiteVentanaV,"contenedor","marciano"+index,50,50,posIni,100);
+                posIni+=100;
+                if (index==9) {
+                    posIni=0;
+                }
+            }else if(index >= 10 && index < 15){
+                this.marciano[index] = new Marciano(this.limiteVentanaH,this.limiteVentanaV,"contenedor","marciano"+index,50,50,posIni,200);
+                posIni+=100;
+                if (index==14) {
+                    posIni=0;
+                }
+            }
+        }
+    }
+
+    moverBloque()//funcion que mueve el los objetos marciano
+    {
+        this.marciano.forEach(element => {
+            element.controlImpactoHorizontal(10);
+        });
+    }
+
+    tocarFondo()//funcion que retorna si cualquier objeto marciano toca el final de la pantalla
+    {
+        let res=false;
+        this.marciano.forEach(element => {
+            if (element.controlImpactoVertical()) 
+            {
+                res = true;
+                return true;
+            }
+        });
+        return res;
+    }
+
+    inicio()
+    {
+        
+       var id = setInterval( () => 
+        {
+            //if (!this.marciano[14].controlImpactoVertical() )//mientras el marciano no toque el final del contenedor 
+            if (!this.tocarFondo()) //mientras el marciano no toque el final del contenedor 
+            {
+                this.moverBloque();
+
+
+                console.log(0);
+            }else{
+                console.log(1);
+                clearInterval(id);//salimos de setInterval si el marciano toca el final
+                
+            }
+
+        },10);
     }
 }
--- clases/Marciano.js
+++ clases/Marciano.js
@@ -2,6 +2,95 @@ export class Marciano
 {
     constructor()
     {
-        
+
+        this.contenedor = document.getElementById(idsvg);//Obtencion del objeto que contendra nuestro marciano.
+        this.marciano = document.createElementNS("http://www.w3.org/2000/svg", "rect");//Creacion del objeto "rect" con SVG.
+
+        this.marciano.setAttribute("id",idc);//Asignacion inicial del ID del objeto.
+
+        this.marciano.setAttribute("width",ancho);//Asignacion de un ancho objeto.
+        this.marciano.setAttribute("height",alto);//Asignacion de un alto objeto.
+
+        this.marciano.setAttribute("x",posX);//Asignacion de una coordenada X objeto.
+        this.marciano.setAttribute("y",posY);//Asignacion de una coordenada Y objeto.
+
+
+        this.marciano.setAttribute("stroke",stroke);//Asignacion inicial del color del borde del objeto. 
+        this.marciano.setAttribute("stroke-width",strokeWidth);//Asignacion inicial del grosor del borde del objeto.
+
+        this.marciano.setAttribute("fill",fill);//Asignacion inicial del color de fondo del objeto.
+
+        this.contenedor.appendChild(this.marciano);//Añadimos el objeto "rect" al obgeto "svg"
+
+        this.direccionH=true;//variable para controlar la direccion del marciano.
+
+        this.limiteVentanaH = limiteVentanaH;
+        this.limiteVentanaV = limiteVentanaV;
+
+        this.ancho = ancho;
+        this.alto = alto;
+
+        this.freno=false;
+
+
+    }
+
+    moverMarcianoDerecha(velocidad=1)//Funcion que incrementando el valor numerico de la coordenada "X" del objeto, desplaza el objeto hacia la derecha.
+    {
+        var x = parseInt(this.marciano.getAttribute('x'));//Obtencion de la coordenada "X" actual del objeto.
+        this.marciano.setAttribute('x', x + velocidad);//Incrementacion e implementacion de la coordenada "X" del objeto.
+    }
+
+    moverMarcianoIzquierda(velocidad=1)//Funcion que decrementando el valor numerico de la coordenada "X" del objeto, desplaza el objeto hacia la izquierda.
+    {
+        var x = parseInt(this.marciano.getAttribute('x'));//Obtencion de la coordenada "X" actual del objeto.
+        this.marciano.setAttribute('x', x - velocidad);//Decrementacion e implementacion de la coordenada "X" del objeto.
+    }
+
+    moverMarcianoArriba(velocidad=1)//Funcion que incrementando el valor numerico de la coordenada "Y" del objeto, desplaza el objeto hacia arriba.
+    {
+        var y = parseInt(this.marciano.getAttribute('y'));//Obtencion de la coordenada "Y" actual del objeto.
+        this.marciano.setAttribute('y', y - velocidad);//Decrementacion e implementacion de la coordenada "Y" del objeto.
+    }
+
+    moverMarcianoAbajo(velocidad=1)//Funcion que incrementando el valor numerico de la coordenada "Y" del objeto, desplaza el objeto hacia abajo.
+    {
+        var y = parseInt(this.marciano.getAttribute('y'));//Obtencion de la coordenada "Y" actual del objeto.
+        this.marciano.setAttribute('y', y + velocidad);//Incrementacion e implementacion de la coordenada "Y" del objeto.
+    }
+
+    retX(){return parseInt(this.marciano.getAttribute('x'));}//Funcion que retorna la coordenada "X" del objeto.
+
+    retY(){return parseInt(this.marciano.getAttribute('y'));}//Funcion que retorna la coordenada "Y" del objeto.
+
+
+    controlImpactoHorizontal(velocidad=1, posX=this.retX() )//Funcion que pasando la altura del contenedor, velocidad de incremento, la coordenada "X" y el radio del objeto hace que este rebote contra los laterales del contenedor.
+    {
+        if(this.direccionH)
+        {
+            this.moverMarcianoDerecha(velocidad);
+            if(posX + this.ancho >= this.limiteVentanaH  )
+            {
+                this.direccionH = false;
+                this.moverMarcianoAbajo(25);
+            }
+            
+        }else if(!this.direccionH){
+            this.moverMarcianoIzquierda(velocidad);
+            if(posX <= 0 )
+            {
+                this.direccionH = true;
+                this.moverMarcianoAbajo(25);
+            }
+        }else{}
+    }
+
+    controlImpactoVertical(posY=this.retY() )//Funcion que pasando la altura del contenedor, velocidad de incremento, la coordenada "Y" y el radio del objeto hace que este rebote contra la parte superior e inferior del contenedor.
+    {
+
+        if( posY + this.alto >= this.limiteVentanaV)
+        {
+            return  true;
+        }
     }
 }
